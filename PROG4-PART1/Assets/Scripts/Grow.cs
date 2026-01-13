186a94175150c2a839216df0f9dbf0234e05a4c8
Giovani Vitti <giovani.mamprin@icloud.com>
2026-01-13 15:49:32 -0500

    Added growth action script

--- PROG4-PART1/Assets/Scripts/Grow.cs
+++ PROG4-PART1/Assets/Scripts/Grow.cs
@@ -1,10 +1,13 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class Grow : ActionTask {
+
+		public float maxSize;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -12,21 +15,22 @@ namespace NodeCanvas.Tasks.Actions {
 			return null;
 		}
 
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
-			EndAction(true);
-		}
+		
+		
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
+			if (agent.transform.localScale.magnitude < maxSize)
+			{
+				agent.transform.localScale += Vector3.one * Time.deltaTime;
+			}
 		}
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
 			
+			agent.transform.localScale = Vector3.one;
 		}
 
 		//Called when the task is paused.
