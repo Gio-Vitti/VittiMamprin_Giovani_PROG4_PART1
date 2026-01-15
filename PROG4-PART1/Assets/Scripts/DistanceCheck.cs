9573ee5d1fd3e7ba0ada0b5aac8ae1b457b285a3
Giovani Vitti <giovani.mamprin@icloud.com>
2026-01-14 21:03:55 -0500

    Linked functionalities together

--- PROG4-PART1/Assets/Scripts/DistanceCheck.cs
+++ PROG4-PART1/Assets/Scripts/DistanceCheck.cs
@@ -1,16 +1,43 @@
+using NodeCanvas.Framework;
+using ParadoxNotion;
+using ParadoxNotion.Design;
 using UnityEngine;
+
 
 public class DistanceCheck : MonoBehaviour
 {
+    public Blackboard blackboard;
+    private bool tooClose;
+    private bool tooFar;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tooClose = blackboard.GetVariableValue<bool>("tooClose");
+        tooFar = blackboard.GetVariableValue<bool>("tooFar");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseTrue()
     {
-        
+        tooClose = true;
+        blackboard.SetVariableValue("tooClose", tooClose);
+    }
+
+    public void CloseFalse()
+    {
+        tooClose = false;
+        blackboard.SetVariableValue("tooClose", tooClose);
+    }
+
+    public void FarTrue()
+    {
+        tooFar = true;
+        blackboard.SetVariableValue("tooFar", tooFar);
+    }
+
+    public void FarFalse()
+    {
+        tooFar = false;
+        blackboard.SetVariableValue("tooFar", tooFar);
     }
 }
