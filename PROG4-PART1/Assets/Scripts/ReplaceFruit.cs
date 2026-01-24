ac3b6c4cf8a090d061e48e771a3bd5ec2ab8f520
Giovani Vitti <giovani.mamprin@icloud.com>
2026-01-23 23:45:37 -0500

    Added employee FSM and functionality

---
+++ PROG4-PART1/Assets/Scripts/ReplaceFruit.cs
@@ -0,0 +1,48 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class ReplaceFruit : ActionTask {
+
+		public float speed;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+			
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+
+			//move forward
+			agent.transform.position += Vector3.right * Time.deltaTime;
+
+			
+            //move back
+            agent.transform.position += Vector3.left * Time.deltaTime;
+
+
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
