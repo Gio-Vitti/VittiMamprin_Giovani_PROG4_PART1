cbb90d07fad6ff98d7ffb5e5311a5bd56816a90e
Giovani Vitti <giovani.mamprin@icloud.com>
2026-01-25 20:06:19 -0500

    Made it so the fruits shrink when spoiled.

--- PROG4-PART1/Assets/Scripts/Shrink.cs
+++ PROG4-PART1/Assets/Scripts/Shrink.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class Grow : ActionTask {
+	public class Shrink : ActionTask {
 
 		public float maxSize;
+		private Vector3 initSize;
+		
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -15,22 +17,25 @@ namespace NodeCanvas.Tasks.Actions {
 			return null;
 		}
 
-		
-		
+		protected override void OnExecute()
+		{
+			initSize = agent.transform.localScale;
+		}
+
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
-			if (agent.transform.localScale.magnitude < maxSize)
+			if (agent.transform.localScale.magnitude > maxSize)
 			{
-				agent.transform.localScale += Vector3.one * Time.deltaTime;
+				agent.transform.localScale -= Vector3.one * Time.deltaTime;
 			}
 		}
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
 			
-			agent.transform.localScale = Vector3.one;
+			agent.transform.localScale = initSize;
 		}
 
 		//Called when the task is paused.
